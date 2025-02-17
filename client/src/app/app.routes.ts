f99bea841963693083ddea0964b75449294870c7
Grayson Goates <Grayson18goates@gmail.com>
2025-02-17 16:34:44 -0600

    about, account, patentdetails components implemented.

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,12 +3,18 @@ import { HomepageComponent } from './Components/homepage/homepage.component';
 import { SignupComponent } from './Components/signup/signup.component';
 import { LoginComponent } from './Components/login/login.component';
 import { WelcomepageComponent } from './Components/welcomepage/welcomepage.component';
+import { AboutComponent } from './Components/about/about.component';
+import { AccountComponent } from './Components/account/account.component';
+import { PatentdetailsComponent } from './Components/patentdetails/patentdetails.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomepageComponent },
     { path: 'signup', component: SignupComponent },
     { path: 'login', component: LoginComponent},
-    {path: 'welcome', component:WelcomepageComponent},
+    {path: 'welcome', component: WelcomepageComponent},
+    { path: 'about', component: AboutComponent},
+    {path: 'account', component: AccountComponent},
+    {path: 'patent', component: PatentdetailsComponent}
 ];
 
 export const appRoutes = RouterModule.forRoot(routes);
