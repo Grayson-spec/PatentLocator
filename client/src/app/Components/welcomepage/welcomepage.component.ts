1a93d499093aa9a8fa382515cb6594967c57a2d1
Grayson Goates <Grayson18goates@gmail.com>
2025-02-17 11:45:02 -0600

    Welcome page implemented.

--- client/src/app/Components/welcomepage/welcomepage.component.ts
+++ client/src/app/Components/welcomepage/welcomepage.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-welcomepage',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './welcomepage.component.html',
   styleUrl: './welcomepage.component.css'
 })
 export class WelcomepageComponent {
+  constructor(private router: Router) { }
 
+  navigateToLogin() {
+    this.router.navigate(['/login']);
+  }
+
+  navigateToSignup() {
+    this.router.navigate(['/signup']);
+  }
 }
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import { SignupComponent } from './Components/signup/signup.component';
+import { LoginComponent } from './Components/login/login.component';
+import { WelcomepageComponent } from './Components/welcomepage/welcomepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomepageComponent },
+    { path: 'signup', component: SignupComponent },
+    { path: 'login', component: LoginComponent},
+    {path: 'welcome', component:WelcomepageComponent},
+];
+
+export const appRoutes = RouterModule.forRoot(routes);
