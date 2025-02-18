bbb791a2a6bd10704be15c847f9ade19fdef6fde
Jdrawert <drawertje@gmail.com>
2025-02-18 16:51:23 -0600

    Update about.component.ts
    test

--- client/src/app/Components/about/about.component.ts
+++ client/src/app/Components/about/about.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
-  imports: [],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
+  constructor(private router: Router) {}
 
+  goBack() {
+    this.router.navigate(['/home']);
+  }
 }
