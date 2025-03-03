5467c8e756808cf09dc19ade4805c00653838592
Grayson Goates <Grayson18goates@gmail.com>
2025-03-03 13:33:14 -0600

    Login page functionality started.
    Ran into a roadblock getting data passed from backend to frontend in proper format. I will likely neet to implement DTOs (Data Transfer Objects) for this to work. Currently, the login button has no functionality.

--- client/src/app/Components/login/login.component.ts
+++ client/src/app/Components/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,14 +8,28 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  constructor(private router: Router) { }
+
+  username = '';
+  password = '';
+  isValid = false;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   onSubmit(username: string, password: string): void {
-    // Add login logic here
-    console.log(username, password);
-    this.router.navigate(['/home']);
+    const user = { Username: username, Password: password };
+    this.http.post('http://localhost:5005/api/users', user)
+      .subscribe((response: any) => {
+        this.isValid = response.isValid;
+        if (this.isValid) {
+          console.log(username, password);
+          this.router.navigate(['/home']);
+        } else {
+          console.log("error");
+        }
+      });
   }
 
+  
   goBack(): void {
     this.router.navigate(['/welcome']);
   }
