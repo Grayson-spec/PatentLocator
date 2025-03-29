20e0ee6a96e2b59cb47c27aae41315f9d7a9e087
Yarwood-cmd <pyarwood@leomail.tamuc.edu>
2025-03-28 21:54:21 -0500

    Begining implementing data base data

--- backend/Models/Patent.cs
+++ backend/Models/Patent.cs
@@ -4,10 +4,11 @@ namespace backend.Models
     public class Patent
     {
         public int Id { get; set; }
+        public required string PatentNumber { get; set; }
         public required string Title { get; set; }
-        public required string Description { get; set; }
+
         public required string Inventor { get; set; }
         public DateTime PublicationDate { get; set; }
-        public required string PatentNumber { get; set; }
+        
     }
 }
