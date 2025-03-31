f2861cf915a71c9043eaff07de813f7931757d94
Yarwood-cmd <pyarwood@leomail.tamuc.edu>
2025-03-30 23:20:29 -0500

    got rid of dead space in code

--- backend/Models/Patent.cs
+++ backend/Models/Patent.cs
@@ -6,7 +6,6 @@ namespace backend.Models
         public int Id { get; set; }
         public required string PatentNumber { get; set; }
         public required string Title { get; set; }
-
         public required string Inventor { get; set; }
         public DateTime PublicationDate { get; set; }
         
